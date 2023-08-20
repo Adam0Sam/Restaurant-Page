5b7801e4de229fe0c41142700b368c46f28ba064
Adam0Sam <132478985+Adam0Sam@users.noreply.github.com>
2023-08-20 13:39:46 +0300

    Added date time

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import UI from './UI.js';
-const content = document.querySelector('.content')
+const content = document.querySelector('.content');
+
+const timeId = document.getElementById('date-text');
+const currentTime = (()=>{
+    let date = new Date();
+    let month = date.getMonth()+1;
+    let day = date.getDate();
+    let year = date.getFullYear();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    timeId.innerText = `${month}-${day}-${year}, ${hh}:${mm}`
+    let t = setTimeout(()=>currentTime(), 60000);
+})();
 
 const menuArray = document.querySelectorAll('.menu-item');
 UI.buttonsToggle(menuArray, 'menu-item-active');
